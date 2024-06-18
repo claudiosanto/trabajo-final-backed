2a9ffd82acf2f16f7267f63c3edd1c3f14c4b5a5
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-06-17 21:19:28 -0300

    listo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express();
 //configuracion de motor de plantilla
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', '../src/views');
+app.set('views', './src/views');
 
 app.use(express.json())
 app.use("/",viewRouter)
@@ -20,5 +20,5 @@ app.use("/api/users",usersRouters)
 
 
 
-app.listen(config.port,() =>console.log (`escuhando al puerto localhost:${config.port}`))
+app.listen(config.port,() =>console.log (`escuhando al puerto http://localhost:${config.port}`))
 
