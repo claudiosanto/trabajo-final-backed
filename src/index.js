18369b7c786d964344f2e0c7b792f6b999d16063
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-06-26 17:29:47 -0300

    areglo de estructuras

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import express from 'express'
 import config from "./config.js";
 import {engine} from 'express-handlebars'
-import usersRouters from "./api/routes/users.routes.js";
-import viewrouter from './api/routes/views.routes.js'
+import usersRouters from "../src/routes/users.routes.js";
+import viewrouter from '../src/routes/views.routes.js'
 const app = express();
 //configuracion de motor de plantilla
 app.engine('handlebars', engine())
 app.set('view engine','handlebars')
 app.set('views' ,'./src/views' )
 
-app.use(express.json())
+app.use(express.json()) 
 app.use(express.static('public'))
 
 app.use('/', viewrouter)
 app.use('/', viewrouter)
 app.use('/', viewrouter)
+
 app.use("api/users/id",usersRouters)
 app.use("/api/users",usersRouters)
 app.use("/api/user/createUser",usersRouters)
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import {  about, home, totalUsers,  } from "../controllers/views.controllers.js"
+import {about, home, totalUsers} from '../controllers/views.controllers.js'
+
 const router = new Router()
 
-router.get('/home', home)
+router.get('/', home )
 router.get('/about', about)
 router.get('/users', totalUsers)
 export default router; 
