70a642fbdb871c6f6036cb72ab38c2aae42bd94f
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-06-30 12:18:18 -0300

    una conexion

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 import express from 'express'
 import config from "./config.js";
-import {engine} from 'express-handlebars'
-import usersRouters from "../src/api/users.routes.js";
+import { engine } from 'express-handlebars'
+import cors from 'cors'
+import usersRouters from "../src/routes/users.routes.js";
 import viewrouter from '../src/routes/views.routes.js'
 import routeUser from '../src/routes/user.routes.js'
 const app = express();
+app.use(cors())
 //configuracion de motor de plantilla
 app.engine('handlebars', engine({
   defaultLayout: 'main',
   layoutsDir: './src/views/layouts',
-  partialsDir:'./src/views/partials'
+  partialsDir: './src/views/partials'
 }))
-app.set('view engine','handlebars')
+app.set('view engine', 'handlebars')
 app.set('views', './src/views')
 
 
-app.use(express.json()) 
+app.use(express.json())
 app.use(express.static('public'))
 
 app.use('/', viewrouter)
 
 app.use('/user', routeUser)
 
-app.use("api/users",usersRouters)
+app.use("/users", usersRouters)
 
 
-app.listen(config.port,() =>console.log (`escuhando al puerto http://localhost:${config.port}`))
+app.listen(config.port, () => console.log(`escuhando al puerto http://localhost:${config.port}`))
 
---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import { createUsers, getAllUsers, getByid } from "../api/users.controllers.js"
+
+const usersRouters = Router()
+
+usersRouters.get("/", getAllUsers)
+usersRouters.get("/id", getByid)
+usersRouters.post("/", createUsers)
+export default usersRouters;
+
