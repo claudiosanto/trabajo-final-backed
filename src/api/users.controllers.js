bddbad23846cf708bd1460c754b24f02ae302e08
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-07-01 17:58:15 -0300

    hola

--- src/api/users.controllers.js
+++ src/api/users.controllers.js
@@ -1,22 +1,22 @@
 import { users } from '../db/users.js'
 
-export const  getAllUsers =  (req, res) => {
+export const getAllUsers = (req, res) => {
   return res.send(users)
 };
 export const getByid = (req, res) => {
   const { id } = req.params.id
-  const existid = users.find(userId => userId.id === Number(id) ) 
+  const existid = users.find(userId => userId.id === Number(id))
   if (!existid) {
-   res.status(404).json("usuario no existe") 
+    res.status(404).json("usuario no existe")
   }
-res.status(200).json("usuarios")
+  res.status(200).json("usuarios")
 }
-export const createUsers=(req, res) => {
-  const { body } = req.body 
-  const limitUsers=users.find(limitUser=>limitUser.name=== body.name)
+export const createUsers = (req, res) => {
+  const { body } = req.body
+  const limitUsers = users.find(limitUser => limitUser.name === body.name)
   if (limitUsers) {
-  res.status(400).json({producto:body, menssage:"usuario ya existe"})
+    res.status(400).json({ producto: body, menssage: "usuario ya existe" })
   }
   users.push(body)
-res.status(200).json("usuario creado")
+  res.status(200).json("usuario creado")
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,7 +4,7 @@ import { createUsers, getAllUsers, getByid } from "../api/users.controllers.js"
 const usersRouters = Router()
 
 usersRouters.get("/", getAllUsers)
-usersRouters.get("/id", getByid)
+usersRouters.get('/id', getByid)
 usersRouters.post("/", createUsers)
 export default usersRouters;
 
