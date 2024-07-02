c5f9017a7e85e41223094049d7e0e63b02611ec9
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-07-02 19:58:07 -0300

    no quiere conectar con el frontd

--- src/api/users.controllers.js
+++ src/api/users.controllers.js
@@ -1,18 +1,21 @@
 import { users } from '../db/users.js'
 
 export const getAllUsers = (req, res) => {
-  return res.send(users)
+  console.log(users)
+  return res.status(200).json(users)
 };
 export const getByid = (req, res) => {
-  const { id } = req.params.id
+  const id = req.params.id
+  console.log(id)
   const existid = users.find(userId => userId.id === Number(id))
   if (!existid) {
-    res.status(404).json("usuario no existe")
+    res.status(404).json('id no encotrado')
   }
-  res.status(200).json("usuarios")
+  res.status(200).json(existid)
+
 }
 export const createUsers = (req, res) => {
-  const { body } = req.body
+  const body = req.body
   const limitUsers = users.find(limitUser => limitUser.name === body.name)
   if (limitUsers) {
     res.status(400).json({ producto: body, menssage: "usuario ya existe" })
