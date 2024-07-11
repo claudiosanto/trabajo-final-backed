662eeec8161e4af236ef4b8b661a59bc7ed768c0
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-07-11 16:58:33 -0300

    dadadadadad

--- src/connect.js
+++ src/connect.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 import config from "./config.js";
 
 export default async function connectDB() {
+  try {
+    await mongoose.connect(config.MONGO_DB_URI)
+  } catch (error) {
 
+  }
 
 
 
