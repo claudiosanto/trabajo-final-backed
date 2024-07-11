8bf55633ca5bcb89b37f0928107006c5d6a867e4
claudiosanto <santosclaudio23acevedo@gmail.com>
2024-07-11 16:55:34 -0300

    holac hico

--- src/connect.js
+++ src/connect.js
@@ -1,13 +1,10 @@
 import mongoose from "mongoose";
 import config from "./config.js";
 
-export default async function connectToDB() {
+export default async function connectDB() {
 
-  try {
-    await mongoose.connect(config.MONGO_DB_URI)
-  } catch (err) {
 
-  }
+
 
 
 }
